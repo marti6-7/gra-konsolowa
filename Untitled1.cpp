ce17da1a03e07343dcf302d190141f84a2b21948
marti6-7 <malinovskaaaaaaa@icloud.com>
2026-06-03 12:42:52 +0200

    opis zmian

---
+++ Untitled1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>//
+#include <cenio.h>//
+#include<cstdlib>//
+#include<ctime> //
+using namespace std;
+
+// wymaganie- tablica dwuwymiarowa, mapa
+char mapa[10][20]
+void generujMape(int graczX, int graY, int celX,int celY){
+}
