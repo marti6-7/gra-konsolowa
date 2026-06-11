72b49aaa1fee73612f0ae1ae6a7b09a76bda2001
marti <ty9njbmbrn@privaterelay.appleid.com>
2026-06-11 13:10:05 +0200

    Update Untitled1.cpp
    poprawki

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -40,21 +40,21 @@ cout << mapa[i][j] << " "; // wypisujemy na ekran znak z tablicy i dodajemy spac
 // rozpoczecie
 int main() {
     // Ekran startowy - wymaganie 10 (wpisz swoje dane)
-    cout << "========================================" << endl; // górną ramka menu
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl; // górną ramka menu
     cout << "  GRA KONSOLOWA: PORUSZANIE PIONKIEM    " << endl; // tytul
-    cout << "  Autor: Jan Kowalski                   " << endl; // imie autora
-    cout << "========================================" << endl; // dolna ramka menu
-    cout << " Instrukcja: Sterowanie za pomoca WASD." << endl;  // instrukcja sterowania
-    cout << " Dotrzyj do celu '$', aby wygrac!" << endl;        // cel gry
-    cout << " Nacisnij dowolny klawisz, aby zaczac...";         // informacja o akcji dla gracza
+    cout << "  projekt zrobiony przez: Marta Malinowska                   " << endl; // moje imie
+    cout << " :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) " << endl; // dolna ramka menu
+    cout << " Instrukcja: Sterowanie za pomoca klawiszy WASD" << endl;  // instrukcja sterowania
+    cout << " musisz dotrzeć do celu '$' aby wygrac!" << endl;        // cel gry
+    cout << " nacisnij dowolny klawisz, aby zaczac :) ";         // informacja o akcji dla gracza
     _getch(); // program czeka na wcisniecie dowolnego klawisza przed startem
 
-    // początkowe współrzędne gracza (wiersz 5,kolumna 5)
+    // początkowe współrzędne gracza 
     int gX = 5;
-    // początkowe współrzędne gracza (Y to kolumna w funkcji)
+    // początkowe współrzędne gracza 
     int gY = 5;
 
-    // współrzędne celu - $ , do którego gracz musi dojść - wiersz 3, kolumna 14
+    // współrzędne celu - $ gdzie gracz musi dojść - wiersz 3 kolumna 14
     int cX = 3;
     int cY = 14;
 
@@ -66,45 +66,45 @@ int main() {
 
         // wywołanie funkcji aby narysowac mape z aktualnymi pozycjami
         generujMape(gX, gY, cX, cY);
-        // Wypisanie aktualnego stanu gry pod mapą
-        cout << endl << "Punkty: " << punkty << " | Pozostalo ruchow: " << limitRuchow << endl; // Wyświetla punkty i ruchy.
-        cout << "Pozycja pionka (X, Y): (" << gX << ", " << gY << ")" << endl; // Wyświetla współrzędne gracza.
+        // wypisanie aktualnego stanu gry pod mapą
+        cout << endl << "Punkty: " << punkty << " | Pozostalo ruchow: " << limitRuchow << endl; // wyswietla punkty i ruchy
+        cout << "Pozycja pionka (X, Y): (" << gX << ", " << gY << ")" << endl; // wys współrzędne gracza
 
-        // Sprawdzenie warunku wygranej - czy gracz stoi na polu z celem
+        // warunek wygranej - czy gracz stoi na polu z celem
         if (gX == cX && gY == cY) {
-            punkty += 10; // Dodajemy punkty za wygraną.
-            cout << endl << "GRATULACJE! Dotarles do celu! Zdobyles " << punkty << " punktow." << endl; // Komunikat o wygranej.
-            break; // Przerywa pętlę i kończy grę.
+            punkty += 10; // dodanie punktu za wygrana
+            cout << endl << "GRATULACJE! Dotarles do celu! Zdobyles " << punkty << " punktow." << endl; // wygrana
+            break; // koniec petli przerwana gra
         }
 
-        // Sprawdzenie warunku przegranej - brak ruchów
+        // sprawdzenie warunku przegranej - brak ruchów
         if (limitRuchow <= 0) {
-            cout << endl << "KONIEC RUCHOW! Przegrales." << endl; // Komunikat o przegranej.
-            break; // Przerywa pętlę i kończy grę.
+            cout << endl << "KONIEC RUCHOW! Przegrales." << endl; // przegrana
+            break; // koniec petli 
         }
 
-        // Oczekiwanie na ruch gracza i pobranie znaku z klawiatury
-        char ruch = _getch(); // Zapisuje wciśnięty klawisz (W, A, S lub D) do zmiennej 'ruch'.
+        // oczekuje na ruch ze strony gracza
+        char ruch = _getch(); // zapisuje wcisnięty klawisz  do zmiennej ruch
 
         // Reakcja na klawisze sterowania i zmiana współrzędnych
         if ((ruch == 'w' || ruch == 'W') && gX > 1) {
-            gX--; // ruch w górę: zmniejszamy numer wiersza (jeśli nie uderzymy w górną ścianę).
-            limitRuchow--; // Każdy ruch zmniejsza limit o 1.
+            gX--; // ruch w górę zmniejszamy numer wiersza esli nie wejdzie w sciane
+            limitRuchow--; // kazdy ruch zmniejsza o 1
         }
         else if ((ruch == 's' || ruch == 'S') && gX < 😎 {
-            gX++; // ruch w dół: zwiększamy numer wiersza (jeśli nie uderzymy w dolną ścianę).
-            limitRuchow--; // Każdy ruch zmniejsza limit o 1.
+            gX++; // ruch w dół: zwiększamy numer wiersza
+            limitRuchow--; // kazdy ruch zmniejsza limit 1
         }
         else if ((ruch == 'a' || ruch == 'A') && gY > 1) {
             gY--; // ruch w lewo: zmniejszamy numer kolumny (jeśli nie uderzymy w lewą ścianę).
             limitRuchow--; // Każdy ruch zmniejsza limit o 1.
         }
         else if ((ruch == 'd' || ruch == 'D') && gY < 18) {
-            gY++; // ruch w prawo: zwiększamy numer kolumny (jeśli nie uderzymy w prawą ścianę).
-            limitRuchow--; // Każdy ruch zmniejsza limit o 1.
+            gY++; // ruch w prawo zwiększamy numer kolumny 
+            limitRuchow--; // kazdy ruch zmniejsza limit o 1
         }
     }
 
-    cout << "Koniec programu. Dziekuje za gre!" << endl; // Napis końcowy po wyjściu z pętli.
-    return 0; // poprawne zakończenie działania funkcji głównej main.
+    cout << " dziękuję za gre " << endl; // napis koncowy
+    return 0; // zakończenie main
 }
